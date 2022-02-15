c2368deea93b6ded8ccd5a312aa0da1f9760a0a9
nely de faria <nelly.nf54@gmail.com>
2022-02-15 14:22:32 -0300

    Criando arquivo exemplo_01.py

---
+++ programacao-rad/aula-02/exemplo_01.py
@@ -0,0 +1,8 @@
+saida = 'Olá mundo do Python'
+print(saida)
+
+saida2 = "Olá mundo do Python. De novo. Esta é uma string maior, cheia de\n" \
+         "linhas. Vamos um pouco mais de texto aleatório apenas para você\n" \
+         "entender como funciona."
+
+print(saida2)
