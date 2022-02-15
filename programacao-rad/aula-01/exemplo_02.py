28d5d2fe39f805c9c1b7c6ec116cdf606540fbe0
nely de faria <nelly.nf54@gmail.com>
2022-02-15 14:20:52 -0300

    Criando arquivo exemplo_02.py

---
+++ programacao-rad/aula-01/exemplo_02.py
@@ -0,0 +1,12 @@
+#  Um comentário
+#  Qualquer coisa após o # é ignorado pelo Python.
+
+print('Eu poderia ter um código como esse')  # e o comentátio depois é ignorado
+
+#  Você também pode usar um comentário para "desativar" ou comentar um pedaço de código
+
+#  print('Isso não será executado')
+
+print('Isso será executado')
+
+print('#Isto será impresso')
