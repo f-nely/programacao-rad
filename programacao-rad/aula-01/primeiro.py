6d89d099d51e1d13f32b0914b10b5cd3fae8ed52
nely de faria <nelly.nf54@gmail.com>
2022-02-15 14:20:05 -0300

    Criando arquivo primeiro.py

---
+++ programacao-rad/aula-01/primeiro.py
@@ -0,0 +1,16 @@
+#  print('Alô mundo!')
+print('Oi de novo')
+#  print('Eu gosto de digitar.')
+print('É bem divertido.')
+#  print('Legal!')
+print("Você conhece joana D'arc?")
+print('Observe "bem" este exemplo!')
+
+'''
+    Python suporta os paradigmas:
+    - Programação Orientada a Objetos;
+    - Programação Estrutura;
+    - Programação Orientada a Aspectos;
+    - Programação Funcional;
+    - Programação Lógica: por meio de algumas extensões.
+'''
