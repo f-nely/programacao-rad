7f18da3fc66e40535c8e051d487f2f375add7444
nely de faria <nelly.nf54@gmail.com>
2022-03-28 18:40:05 -0300

    Criando arquivo files.py

---
+++ programacao-rad/aula-08/files.py
@@ -0,0 +1,25 @@
+# arquivo = open('contatos.txt', 'a')
+# arquivo.write('Olá, mundo!\n')
+#
+# frases = list()
+# frases.append('TreinaWeb \n')
+# frases.append('Python \n')
+# frases.append('Arquivos \n')
+# frases.append('Django \n')
+# arquivo.writelines(frases)
+
+# arquivo = open('contatos.txt', 'r')
+# print(arquivo.readline(3))
+# print(arquivo.readlines())
+
+# arquivo.close()
+
+# escrevendo
+# f = open('test.txt', 'w')
+# f.write('texto 1')
+# f.close()
+
+# lendo
+f = open('test.txt', 'r')
+print(f.read())
+f.close()
