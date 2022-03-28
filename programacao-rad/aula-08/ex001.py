43236cb109f6860a23655d805000e2f93ef18ee4
nely de faria <nelly.nf54@gmail.com>
2022-03-28 18:41:04 -0300

    Criando arquivo ex001.py

---
+++ programacao-rad/aula-08/ex001.py
@@ -0,0 +1,12 @@
+"""
+arquivos
+ - Quando abrimos um arquivo para leitura, se não existe, ocorre erro.
+ - Quando abrimos um arquivo para leitura, se não existe, um novo é criado.
+ - Quando abrimos um arquivo para escrita, se existe, é sobrescrito com o novo.
+"""
+str1 = 'python'
+print(str1.center(15, '*'))
+
+str2 = 'abababadaadbbaccabc'
+print(len(str2))
+print(str2.count('ab', -17, -1))
