95481aef802bb5a3076c0a36e2f261afa627dc79
nely de faria <nelly.nf54@gmail.com>
2022-03-31 13:13:10 -0300

    Criando arquivo teste.py

---
+++ programacao-rad/aula-10/teste.py
@@ -0,0 +1,6 @@
+# import transportes.logistica.frete
+# transportes.logistica.frete.teste()
+# from transportes.logistica import frete
+# frete.teste()
+from transportes.logistica.frete import teste
+teste()
