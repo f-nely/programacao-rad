440d23e4aa5a1cd6db55e353daa58fc1372f9712
nely de faria <nelly.nf54@gmail.com>
2022-03-31 13:12:10 -0300

    Criando arquivo app.py

---
+++ programacao-rad/aula-10/modulos/app.py
@@ -0,0 +1,15 @@
+import transportes
+
+"""
+import transportes
+neste caso estamos importando TUDO que estiver dentro módulo
+
+from transportes import distancia
+neste caso, do módulo transportes.py só valos importar função
+distancia(), as outras não seram importadas
+
+from transpoetes import *
+neste caso, importa tudo que estiver no módulo
+"""
+
+transportes.distacia()
