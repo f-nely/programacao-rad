3fe45dc6f567e42d3d483727cbb0fa9949315a5b
nely de faria <nelly.nf54@gmail.com>
2022-03-31 13:11:43 -0300

    Criando arquivo transportes.py

---
+++ programacao-rad/aula-10/modulos/transportes.py
@@ -0,0 +1,16 @@
+"""
+    módulo - é um arquivo que contém código Python, no qual é possível
+    encontrar definições de classes, funções e até sequências de comandos.
+"""
+
+
+def calcula_frete():
+    pass
+
+
+def calcula_imposto():
+    pass
+
+
+def distacia():
+    pass
