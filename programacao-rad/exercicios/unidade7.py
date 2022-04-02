bdd8df8ba141204cf13bd29a6a7fb7d35d2e7a06
nely de faria <nelly.nf54@gmail.com>
2022-04-02 13:50:20 -0300

    Criando arquivo unidade7.py

---
+++ programacao-rad/exercicios/unidade7.py
@@ -0,0 +1,11 @@
+estrutura = (3, 4, 6, 5, 'b', 'f', 5, 8, 2)
+print(type(estrutura), estrutura.count(5))
+print('-=' * 10)
+
+"""
+Dicionários - métodos:
+get() - método utilizado para obter o conteúdo associado à chave
+value() - Permite visualizar os valores armazenados.
+key() - Permite identificar as chaves de um dicionário.
+items() - Permite retornar os elementos na forma de tuplas.
+"""
