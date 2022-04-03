513862a8d7f6027348a3bdc74a5b4c745241d03d
nely de faria <nelly.nf54@gmail.com>
2022-04-03 18:52:19 -0300

    Inserindo exercício

--- programacao-rad/exercicios/unidade2.py
+++ programacao-rad/exercicios/unidade2.py
@@ -4,3 +4,13 @@ print(int('1000') + 1000)
 
 str1 = 'Python'
 print(str1[0])
+print('-=' * 10)
+
+valor1 = input('Digite um número: ')
+valor2 = input('Digite um outro número: ')
+
+resultado = float(int(valor1) + int(valor2)) / 2
+print(resultado)
+print('-=' * 10)
+
+print(float('20') + 2)
